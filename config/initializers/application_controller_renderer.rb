238c0cda7cefcdce541af9e56edc6dc9329cdcfd
EC2 Default User <ec2-user@ip-172-31-42-189.ap-northeast-1.compute.internal>
2022-11-12 04:39:07 +0000

    Update Gemfile for Heroku

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '886dcaf86ae3861f95a4fa403f6d3787b1abff652649af108ff070a88480be10a1cd63f1b1c12d9c72d550f071f65044ac4e0449b9c156214276cbd21d62e82c'
